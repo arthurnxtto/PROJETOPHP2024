b7192dff94afe104946ea5bba1f2ba5cbbce6e1d
arthurnxtto <arthurvnetto8@outlook.com>
2024-03-20 22:09:48 -0300

    criando funçoes

---
+++ calculadora.php
@@ -0,0 +1,42 @@
+<?php 
+
+$num1 = 20.50
+$num2 = 30.50
+
+function soma($num1, $num2){
+	return ($num1 + $num2);
+
+
+}
+	
+
+	function subtracao($num1, $num2){
+	return ($num1 - $num2);
+
+
+}
+	
+
+	function divisao($num1, $num2){
+	return ($num1 / $num2);
+
+
+}
+	
+
+	function multiplicacao($num1, $num2){
+	return ($num1 * $num2);
+
+
+}
+	
+
+
+
+
+
+
+
+
+
+ ?>
---
+++ exemplo09.php
@@ -0,0 +1,12 @@
+<?php 
+
+function calcular($num1, $num2){
+	return ($num1 + $num2);
+
+
+}
+	echo calcular(10,20);
+
+
+
+ ?>
