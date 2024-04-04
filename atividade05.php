caf22f634d77a419aba1f6ef1634ebb83f3ff860
arthurnxtto <149740203+arthurnxtto@users.noreply.github.com>
2024-04-03 22:15:33 -0300

    Add files via upload

---
+++ atividade05.php
@@ -0,0 +1,40 @@
+<?php 
+
+	$idade1 = 18;
+	$idade2 = 14;
+	$idade3 = 17;
+	$idade4 = 16;
+	$idade5 = 15;
+
+
+	$altura1 = 1.50;
+	$altura2 = 1.40;
+	$altura3 = 1.60;
+	
+
+
+	for($pessoa = 0; $pessoa <= 5; $pessoa++){
+
+			if ($altura1 > $altura2 && $altura1 > $altura3 ) {
+
+			echo "Maior altura é $altura1";
+
+		}
+		elseif ($altura1 > $altura2 && $altura1 > $altura3 ) {
+
+			echo "Maior altura é $altura1";
+ 
+		}
+	}
+
+
+	$media = 0;
+
+
+
+
+
+
+
+
+ ?>
