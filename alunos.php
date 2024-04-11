37a07b40aa982d7cbcd4904cec9ff66917e990c5
arthurnxtto <149740203+arthurnxtto@users.noreply.github.com>
2024-04-10 22:16:32 -0300

    Add files via upload

---
+++ alunos.php
@@ -0,0 +1,30 @@
+<?php 
+	
+	//$aluno1 = 'Arthur';
+	//$aluno2 = 'Arthur';
+	//$aluno3 = 'Arthur';
+	//$aluno4 = 'Arthur';
+	//$aluno5 = 'Arthur';
+	//$aluno6 = 'Arthur';
+	
+	for ($c = 0; $c <= 6; $c++) {
+		$aluno1 = 0;
+		$aluno1++;
+		
+		
+	$num1 = 0;
+	$num2 = 0;
+	function media($num1, $num2){
+	return ($num1 + $num2 ) / 2;
+	
+} 
+	
+	echo "$aluno1 teve a media <br />" . media(10,10) ;
+	
+	
+
+	}
+
+	
+
+ ?>	
