402ac130b1099a3e800025532ed6d4f5974cd436
arthurnxtto <149740203+arthurnxtto@users.noreply.github.com>
2024-04-24 22:15:19 -0300

    Add files via upload

---
+++ exemplos10.php
@@ -0,0 +1,17 @@
+<?php 
+		
+$nome = array("Maria", "João", "Pedro");
+echo "Os nomes no array:";
+
+foreach ($nome as $nomes) {
+	echo "<br /> $nomes";
+}
+
+$num = array(1,2,3,4,5);
+
+foreach( $num as $valor) {
+
+	echo "Valoe é $valor <br />";
+}
+
+ ?>
